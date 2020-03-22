dc79d6c371f51bbeba3910e841a2e14aa824baad
Aslam Doctor <aslam.doctor@gmail.com>
2020-03-22 12:53:33 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const port = 3000
+const port = process.env.POST || 3000
 
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
